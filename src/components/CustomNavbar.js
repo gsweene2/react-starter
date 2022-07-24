9e8363f3004be014241bf31f94baad42b65ec30e
Garrett Sweeney <garrett.d.sweeney@gmail.com>
2022-07-24 19:20:49 -0400

    Routing + React Bootstrap Styling

---
+++ src/components/CustomNavbar.js
@@ -0,0 +1,29 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { Link } from 'react-router-dom';
+
+function CustomNavbar() {
+  return (
+    <>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand as={Link} to="/">Company Name</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link as={Link} to="/">
+                Home
+            </Nav.Link>
+            <Nav.Link as={Link} to="/about">
+                About
+            </Nav.Link>
+            <Nav.Link as={Link} to="/users">
+                Users
+            </Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </>
+  );
+}
+
+export default CustomNavbar;
